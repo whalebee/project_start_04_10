8e8330ab276bac240d1da01c50438ba820af2b28
whalebee <islhoeve@gmail.com>
2023-05-26 11:27:28 +0900

    05-26 class-3

--- A-디버그용/햄버거먹고싶다.c
+++ A-디버그용/햄버거먹고싶다.c
@@ -1,27 +1,36 @@
-#include "header.h"
+// #include "header.h"
+#include <stdio.h>
 
 
 int main()
 {
 
+
+//		  (1,3)
+//	(2,3) (2,4) (2,6)
+
+
 	// n단트리 for문 2번만 사용해서
 
 	int n = 3;
+	scanf("%d", &n);
 
-	for (int row = 1; row < n; row++)
+	for (int row = 1; row <= n; row++)
 	{
 
 		for (int col = 1; col < n + row; col++)
 		{
-			if (col < n - row)
+			if (col > n - row)
 			{
-				printf("* ");
+				printf("*");
 			}
 			else
 			{
-				printf("g");
+				printf(" ");
 			}
 		}
+
+		printf("\n");
 	}
 
 
