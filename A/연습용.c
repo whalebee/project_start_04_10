e1c649c4b36867b1c6482775f7888455d1ec33ec
Whalebee <127908829+Whalebee@users.noreply.github.com>
2023-06-23 00:01:10 +0900

    06-22 복습

--- A/연습용.c
+++ A/연습용.c
@@ -7,6 +7,19 @@
 int main()
 {
 
+	// A , Z 해보셈
+
+	int A, Z, result;
+
+	for (A = 0; A < 10; A++)
+	{
+		for (Z = 0; Z < 10; Z++)
+		{
+			result = (A * 10 + Z) + (Z * 10 + A);
+			if (result == 99)
+				printf("%d%d + %d%d = %d \n", A, Z, Z, A, result);
+		}
+	}
 	
 
 
